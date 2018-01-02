985799a1fce06806c6dca3870f232bf6a3b33664
capitalmarkettools <capitalmarkettools.org@gmail.com>
2018-01-01 21:40:06 -0500

    installed ccxt and tested

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ Created on 1 Jan 2018
 @author: cmt
 '''
 
+import ccxt
+
 print 'Start'
+print 'Supported exchanges:'
+print(ccxt.exchanges)
+
+hitbtc = ccxt.hitbtc({'verbose': False})
+hitbtc_markets = hitbtc.load_markets()
+
+print('HITBTC markets:')
+print('hitbtc:\n', hitbtc.id, hitbtc_markets)
+
+print('All sympols')
+for symbol in hitbtc.symbols:
+    print symbol
+    
+print('Order book for ', hitbtc.symbols[0])
+print(hitbtc.fetch_order_book(hitbtc.symbols[0]))
+
 print 'End'
 
 if __name__ == '__main__':
